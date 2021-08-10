11eb9b860d45f33bcb935547df97e854b2fc8e82
SerhiiValchuk <valthuk@yahoo.com>
2021-08-10 20:02:44 +0300

    classwork1

--- src/components/adrees/Address.js
+++ src/components/adrees/Address.js
@@ -1,10 +1,13 @@
-export default function Address({street, suite, city, zipcode}) {
+import Geo from "../geo/Geo";
+
+export default function Address({street, suite, city, zipcode, geo}) {
     return (
         <div>
             <h3>{street}</h3>
             <h3>{suite}</h3>
             <h3>{city}</h3>
             <h3>{zipcode}</h3>
+            <Geo {...geo}/>
         </div>
     );
 }
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,7 +1,8 @@
 import Address from "../adrees/Address";
+import Company from "../company/Company";
 
 
-export default function Users({id, name, username,email,phone, website }) {
+export default function Users({id, name, username,email,phone, website, address, company }) {
     return (
         <div>
             <h2>{id}</h2>
@@ -10,6 +11,8 @@ export default function Users({id, name, username,email,phone, website }) {
             <h2>{email}</h2>
             <h2>{phone}</h2>
             <h2>{website}</h2>
+            <Address {...address}/>
+            <Company {...company}/>
         </div>
     );
 }
