c9f841a87227b8d5aba72dcc3e87c7def51482d3
SerhiiValchuk <valthuk@yahoo.com>
2021-08-12 17:53:06 +0300

    classwork2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Users from "./components/users/Users";
 
 export default function App() {
   return (
     <div>
-
+      <h1>
+        <Users/>
+      </h1>
     </div>
   );
 }
---
+++ src/components/user/User.js
@@ -0,0 +1,7 @@
+export default function User({item:userItem}) {
+    return (
+        <div>
+            {userItem.id} {userItem.name}
+        </div>
+    );
+}
