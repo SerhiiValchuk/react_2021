2e9572f141871e8dc0a70e09aa880d8355441295
SerhiiValchuk <valthuk@yahoo.com>
2021-08-16 13:56:49 +0300

    classwork2

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,10 +1,11 @@
+import Comments from "../comments/Comments";
+
 export default function Posts({items}) {
   return (
     <div>
       {
         items.map(value =><div>{value.title}</div>)
       }
-
     </div>
   );
 }
