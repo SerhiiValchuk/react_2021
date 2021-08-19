31074f185c30a405108075bc98d67c9c4b2a98cf
SerhiiValchuk <valthuk@yahoo.com>
2021-08-19 18:48:02 +0300

    class3

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,23 +1,22 @@
 import Post from "../post/Post";
 import {getPosts} from "../../services/user.fetch.service";
-import {useEffect, useState} from "react";
+import {useState} from "react";
+
 export default function User({item: userItem}) {
     let [posts, setPosts] = useState([]);
-    useEffect(()=>{
-        getPosts().then(({data}) => setPosts([...data]));
 
-    },[]);
+    const getUserPost = () => {
+        getPosts(userItem.id).then(data => setPosts([...data]));
+    };
 
     return (
         <div>
             {userItem.id} {userItem.name}
-            <button onClick={() => {
-            }}>posts
-            </button>
-            <div>{
-                posts.map((item) =>
-                <Post item = {item}/>)
-            }
+            <button onClick={getUserPost}>posts</button>
+            <div>
+                {
+                    posts.map((item) => <Post item={item}/>)
+                }
             </div>
         </div>
     );
