49e85a1031896920a7ae7a46635e7ae8f61a5297
SerhiiValchuk <valthuk@yahoo.com>
2021-09-03 15:10:51 +0300

    home4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Form from "./components/form/Form";
 
 export default function App() {
   return (
     <div>
-
+     <Form/>
     </div>
   );
 }
---
+++ src/components/form/Form.js
@@ -0,0 +1,13 @@
+export default function Form() {
+  return (
+    <div>
+    <form>
+        <input type="text" name={'model'}/>
+        <input type="text" name={'price'}/>
+        <input type="text" name={'year'}/>
+        <button>save</button>
+    </form>
+
+    </div>
+  );
+}
